53cc1604ae47176ffb83ffe8d2e82462e19969b1
Joe Binns <joebinns.95@gmail.com>
2023-02-04 17:27:44 +0100

    Player hold rotation working, but input needs to change space

--- GP2_Team09/Assets/Complete/Game Systems/Controller/Hold/PlayerHoldRotation.cs
+++ GP2_Team09/Assets/Complete/Game Systems/Controller/Hold/PlayerHoldRotation.cs
@@ -6,24 +6,29 @@ using UnityEngine;
 
 namespace GameProject.Hold
 {
-    [RequireComponent(typeof(PlayerHold))]
+    [RequireComponent(typeof(PlayerHold), typeof(PlayerLook), typeof(PlayerTurn))]
     public class PlayerHoldRotation : BaseAction
     {
         [SF] private PlayerController _controller = null;
         [Space]
         [SF] private MovementSettings _settings = null;
         [SF] private InputManager _input = null;
-
+        
         private PlayerHold _playerHold;
+        private PlayerLook _playerLook;
+        private PlayerTurn _playerTurn;
         private Transform _holdPivot;
-        private float _angle = 0;
-        private float _direction = 0;
+        private Vector2 _angle;
+        private Vector2 _direction;
         private bool _isRotatable;
         
 // INITIALISATION
 
         private void Awake() {
             _playerHold = GetComponent<PlayerHold>();
+            _playerLook = GetComponent<PlayerLook>();
+            _playerTurn = GetComponent<PlayerTurn>();
+            
             _holdPivot = _playerHold.HoldPivot;
         }
 
@@ -34,6 +39,7 @@ namespace GameProject.Hold
             _controller.AddAction(this, UpdateMode.LateUpdate);
             _input.SubscribeKey(OnSecondaryInput, InputType.Secondary, Priority);
             _input.SubscribeFloat(OnLookInput, InputType.Tilt, Priority);
+            _input.SubscribeFloat(OnTurnInput, InputType.Turn, Priority);
         }
 
         /// <summary>
@@ -43,6 +49,7 @@ namespace GameProject.Hold
             _controller.RemoveAction(this, UpdateMode.LateUpdate);
             _input.UnsubscribeKey(OnSecondaryInput, InputType.Secondary);
             _input.UnsubscribeFloat(OnLookInput, InputType.Tilt);
+            _input.UnsubscribeFloat(OnTurnInput, InputType.Turn);
         }
         
 // INPUT
@@ -51,15 +58,23 @@ namespace GameProject.Hold
         /// Updates look direction on input callback
         /// </summary>
         private void OnLookInput(float direction){
-            _direction = -direction;
+            _direction.y = -direction;
+        }
+        
+        /// <summary>
+        /// Updates turn direction on input callback
+        /// </summary>
+        private void OnTurnInput(float direction){
+            _direction.x = direction;
         }
         
         private void OnSecondaryInput(){
             if (_playerHold.IsHolding) _isRotatable = !_isRotatable;
             else _isRotatable = false;
-
-            GetComponent<PlayerLook>().enabled = !_isRotatable;
+            
             //_input.SetInputState(InputType.Tilt, !_isRotatable); this disabled tilt input for both look and rotation
+            GetComponent<PlayerLook>().enabled = !_isRotatable;
+            GetComponent<PlayerTurn>().enabled = !_isRotatable;
         }
         
 // MOVEMENT
@@ -68,15 +83,17 @@ namespace GameProject.Hold
         /// Checks look input on controller update
         /// </summary>
         public override bool OnCheck() {
-            return _isRotatable && _direction != 0;
+            return _isRotatable && _direction != Vector2.zero;
         }
 
         /// <summary>
         /// Rotate hold pivot on controller update
         /// </summary>
-        public override void OnUpdate(float deltaTime) {
-            _angle += _direction * _settings.LookSensi;
-            _holdPivot.localRotation = Quaternion.Euler(_angle, 0, 0);
+        public override void OnUpdate(float deltaTime)
+        {
+            _angle.x += _direction.y * _settings.LookSensi;
+            _angle.y += _direction.x * _settings.TurnSensi;
+            _holdPivot.localRotation = Quaternion.Euler(_angle.x, _angle.y, 0);
         }
     }
 }
